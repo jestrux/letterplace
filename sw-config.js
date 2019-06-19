21bd5697cc0e1d56227bbbb76b85901ebac01c94
Walter Kimaro <wakyj07@gmail.com>
2019-06-19 20:10:23 +0300

    Add sw-precache-cra to cache extra files

---
+++ sw-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  staticFileGlobs: [
+    './build/**/**.html',
+    './build/static/js/*.js',
+    './build/static/css/*.css',
+    './build/static/media/**',
+    './build/logo.png',
+    './build/icon.png',
+    './build/apple-touch-icon.png',
+    './build/favicon-32x32.png',
+    './build/favicon-16x16.png',
+    './build/safari-pinned-tab.svg',
+    'Dictionary.js'
+  ]
+};
