e023ddfdc0bec5f2e33cbf45c7a3ad835ff969b4
Walter Kimaro <wakyj07@gmail.com>
2019-06-19 19:26:34 +0300

    Create isSurrounded helper method, generalize getTilesImage and generalize getTileBg method

--- src/LetterPlaceHelpers.js
+++ src/LetterPlaceHelpers.js
@@ -1,5 +1,4 @@
 import * as tinycolor from 'tinycolor2';
-
 export const themes = {
 	0 : ['#8bc34a', '#5d4037'],
 	1 : ['#00bcd4', '#e91e63'],
@@ -22,7 +21,7 @@ export const getTilesImage = function(tiles, colors){
 		var y = Math.floor(i / 5)*10 + space;
 
 		var tile = tiles[i];
-		var fillColor = _getTileBg(colors, tile.owner, tile.locked, defaultColor);
+		var fillColor = getTileBg(colors, tile.owner, tile.locked, defaultColor);
 		
 		ctx.fillStyle = fillColor;
 		ctx.fillRect(x, y, (10 - space), (10 - space));
@@ -30,17 +29,22 @@ export const getTilesImage = function(tiles, colors){
 
 	colors.splice(2, 1);
 
-	return canvas.toDataURL();
+	return canvas.toDataURL().replace("data:image/png;base64,", "");
 }
 
-function _getTileBg(colors, owner, locked, defaultColor){
+export const getTileBg = (...args) => {
+	if(args.length === 2){
+		const [color, locked] = args;
+		return locked ? tinycolor(color).darken(10).toHexString() : color;
+	}
+	const [colors, owner, locked, defaultColor] = args;
 	if(owner !== -1){
 		var color = colors[owner];
 
 		if(!locked)
 			return color;
 		else
-			return tinycolor(color).darken(15).toHexString();
+			return tinycolor(color).darken(10).toHexString();
 	}
 	else
 		return defaultColor;
@@ -89,8 +93,6 @@ export const getRandomTiles = function(){
 	for (var i = 0; i < 25; i++) {
 			lettersArray[i] = {
 				owner  : -1,
-				locked : false,
-				played : false,
 				row : Math.floor(i / 5),
 				column : i % 5,
 				letter : _getRandomLetter().toUpperCase()
@@ -148,4 +150,40 @@ function _getRandomLetter(){
 		else
 			sum += frequencies[letter];
 	}
+}
+
+function _tileOnMatrix(tiles, row, col){
+	var rowOutOfBound = row < 0 || row > 4;
+	var colOutOfBound = col < 0 || col > 4;
+	if(rowOutOfBound || colOutOfBound)
+		return null;
+
+	var index = (row * 5) + col;
+	return tiles[index];
+}
+
+export const isSurrounded = (tiles, index) => {
+	var tile = tiles[index];
+	var tileOwner = tile.owner;
+	if(tileOwner === -1){
+		return false;
+	}
+	var trow = Math.floor(index / 5);
+	var tcol = index - (trow * 5);
+	var tileAbove = _tileOnMatrix(tiles, trow - 1, tcol);
+	var tileBelow = _tileOnMatrix(tiles, trow + 1, tcol);
+	var tileBefore = _tileOnMatrix(tiles, trow, tcol - 1);
+	var tileAfter = _tileOnMatrix(tiles, trow, tcol + 1);
+
+	var onLeft = tcol === 0;
+	var onRight = tcol === 4;
+	var onTop = trow === 0;
+	var onBottom = trow === 4;
+
+	var topCovered = onTop || (tileAbove && tileAbove.owner === tileOwner);
+	var leftCovered = onLeft || (tileBefore && tileBefore.owner === tileOwner);
+	var rightCovered = onRight || (tileAfter && tileAfter.owner === tileOwner);
+	var bottomCovered = onBottom || (tileBelow && tileBelow.owner === tileOwner);
+	
+	return topCovered && bottomCovered && rightCovered && leftCovered;
 }
