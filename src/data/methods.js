03718a7c059004da3647252efd4e65c4ea062d7d
Walter Kimaro <wakyj07@gmail.com>
2019-06-24 13:58:49 +0300

    Increase priority of notification

--- src/data/methods.js
+++ src/data/methods.js
@@ -7,7 +7,7 @@ const isLocalhost = Boolean(
     window.location.hostname === '[::1]' ||
     // 127.0.0.1/8 is considered localhost for IPv4.
     window.location.hostname.match(
-    /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
     )
 );
 
@@ -17,72 +17,70 @@ export const showGameOverMessage = (game, userId) => {
     const curUserIdx = game.players.indexOf(userId);
     const otherPlayer = curUserIdx === 0 ? game.player2 : game.player1;
     let winIndex = 0;
-    if(game.player2.points > game.player1.points)
+    if (game.player2.points > game.player1.points)
         winIndex = 1;
     const winner = winIndex === curUserIdx ? "You" : otherPlayer.name;
 
     alert(`Game Over! ${winner} won, final score: ${game.player1.points} - ${game.player2.points}`);
 }
 
-export const getGameById = function(gameId){
-	return new Promise(async (resolve, reject) => {
-		db.doc("games/" + gameId).get().then(doc => {
-			if (doc.exists)
-                resolve(doc.data());
-			else
-				reject("Game not found.");
-		})
-		.catch(function(error) {
-            reject(error);
-		})
-	});
+export const getGameById = function (gameId) {
+    return new Promise(async (resolve, reject) => {
+        db.doc("games/" + gameId).get().then(doc => {
+                if (doc.exists)
+                    resolve(doc.data());
+                else
+                    reject("Game not found.");
+            })
+            .catch(function (error) {
+                reject(error);
+            })
+    });
 }
 
-const getUserFCMTokens = function(userId){
-	return new Promise(async (resolve, reject) => {
+const getUserFCMTokens = function (userId) {
+    return new Promise(async (resolve, reject) => {
         try {
             const tokenSnapshot = await db.collection("users/" + userId + "/tokens").get();
-            if(tokenSnapshot.empty){
+            if (tokenSnapshot.empty) {
                 reject("No tokens found!");
                 return;
             }
 
             const tokenDocs = tokenSnapshot.docs;
             const tokens = tokenDocs.map(doc => doc.data());
-            resolve(tokens); 
-        } 
-        catch (error) {
+            resolve(tokens);
+        } catch (error) {
             console.log("Error getting other user token:", error);
             reject(error);
         }
-	});
+    });
 }
 
 export const setUserFcmToken = (userId) => {
     console.log("Setting fcm token....");
     return new Promise(async (resolve, reject) => {
-        messaging.getToken().then(function(token) {
-            if (token) {
-                const tokenData = {
-                    createdAt: new Date().getTime(),
-                    platform: "web",
-                    token
-                };
-                db.doc("users/" + userId + "/tokens/" + token)
-                    .set(tokenData).then(() => {
-                        console.log("Auth token persisted");
-                        resolve();
-                    });
-            } 
-            else {
-                console.log('No Instance ID token available. Request permission to generate one.');
-                reject('No Instance ID token available. Request permission to generate one.');
-            }
-        })
-        .catch(function(err) {
-            console.log('An error occurred while retrieving token. ', err);
-            reject(err);
-        });
+        messaging.getToken().then(function (token) {
+                if (token) {
+                    const tokenData = {
+                        createdAt: new Date().getTime(),
+                        platform: "web",
+                        token
+                    };
+                    db.doc("users/" + userId + "/tokens/" + token)
+                        .set(tokenData).then(() => {
+                            console.log("Auth token persisted");
+                            resolve();
+                        });
+                } else {
+                    console.log('No Instance ID token available. Request permission to generate one.');
+                    reject('No Instance ID token available. Request permission to generate one.');
+                }
+            })
+            .catch(function (err) {
+                console.log('An error occurred while retrieving token. ', err);
+                reject(err);
+            });
     });
 }
 
@@ -91,7 +89,15 @@ const sendFcmNotification = async (userId, notification) => {
     const sendTokenPromises = otherPlayerTokens.map(tokenData => {
         const notificationData = {
             ...notification,
-            "to": tokenData.token
+            "to": tokenData.token,
+            "apns": {
+                "headers": { "apns-priority": "10" }
+            },
+            "webpush": {
+                "headers": { "Urgency": "high" }
+            },
+            "android": { "priority": "high" },
+            "priority": 10
         };
 
         return axios({
@@ -108,14 +114,14 @@ const sendFcmNotification = async (userId, notification) => {
     return Promise.all(sendTokenPromises);
 }
 
-export const sendTurnNotification = async function(player, otherPlayerId, game){
+export const sendTurnNotification = async function (player, otherPlayerId, game) {
     try {
         const score = game.player1.points + " - " + game.player2.points;
         const notification = {
             "notification": {
                 "title": "Your Turn",
                 "body": `${player} played ${game.lastword}, new score: ${score}`,
-                "icon": "data:image/png;base64,"+game.summary_image,
+                "icon": "data:image/png;base64," + game.summary_image,
                 // "click_action": APP_URL + "?source=notification/#view/" + game.id
                 "click_action": APP_URL
             },
@@ -131,13 +137,13 @@ export const sendTurnNotification = async function(player, otherPlayerId, game){
     }
 }
 
-export const sendNewGameNotification = async function(player, otherPlayerId, game){
+export const sendNewGameNotification = async function (player, otherPlayerId, game) {
     try {
         const notification = {
             "notification": {
                 "title": "New Game",
                 "body": `${player} played ${game.lastword}`,
-                "icon": "data:image/png;base64,"+game.summary_image,
+                "icon": "data:image/png;base64," + game.summary_image,
                 // "click_action": APP_URL + "?source=notification/#view/" + game.id
                 "click_action": APP_URL
             },
