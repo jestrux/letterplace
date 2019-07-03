fe42437ccaf4ba7932d0f79a290967efa456d4b6
Walter Kimaro <wakyj07@gmail.com>
2019-07-03 09:33:23 +0300

    Upfate manifest and service worker so notifications can be opened in PWA

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -20,9 +20,30 @@ messaging.setBackgroundMessageHandler(function(payload) {
       icon: 'https://letterplace.herokuapp.com/static/media/logo.f81f4c25.png'
     };
 
-    self.addEventListener('notificationclick', function(){
-      clients.openWindow('http://letterplace.herokuapp.com');
-    })
+    // self.addEventListener('notificationclick', function(){
+    //   clients.openWindow('http://letterplace.herokuapp.com');
+    // });
+
+    self.addEventListener('notificationclick', function (event){
+      //For root applications: just change "'./'" to "'/'"
+      //Very important having the last forward slash on "new URL('./', location)..."
+      const rootUrl = new URL('./', location).href; 
+      event.notification.close();
+      event.waitUntil(
+          clients.matchAll().then(matchedClients =>
+          {
+              for (let client of matchedClients)
+              {
+                  if (client.url.indexOf(rootUrl) >= 0)
+                  {
+                      return client.focus();
+                  }
+              }
+
+              return clients.openWindow(rootUrl).then(function (client) { client.focus(); });
+          })
+      );
+    });
   
     return self.registration.showNotification(
       notificationTitle, 
